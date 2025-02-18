04105397bc1d41d03801c41dd5bd18d124860c47
Teaforks <teaforks@gmail.com>
2025-02-18 17:23:45 +0100

    Hola mundo v1.2 -Añadido un mensaje extra

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,10 +10,13 @@ class Main extends JFrame{
     public Main(){
         JLabel lblSaludo = new JLabel("Hola Mundo.");
         JLabel lblSaludo2 = new JLabel("¿Qué tal estás?.");
+        JLabel lblSaludo3 = new JLabel("¡Espero que bien!");
         add(lblSaludo);
         add(lblSaludo2);
+        add(lblSaludo3);
         lblSaludo.setBounds(2,28,116,24);
         lblSaludo2.setBounds(2,50,116,24);
+        lblSaludo3.setBounds(2,67,116,24);
         this.setSize(400,300);
         this.setTitle("JFrame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
